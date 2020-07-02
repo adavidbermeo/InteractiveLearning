4ce6bde8546b58f9e1fb3658407f51146a2fa754
Ángel David Bermeo Penagos <54322475+adavidbermeo@users.noreply.github.com>
2020-07-02 12:49:17 -0500

    Angel Commit v2

--- app/src/main/java/com/example/interactivelearning/MainActivity.java
+++ app/src/main/java/com/example/interactivelearning/MainActivity.java
@@ -14,14 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        et1 = (EditText)findViewById(R.id.et1);
     }
 
     //Método para el botón enviar
-    public void Enviar(View view){
+    public void createAccount(View view){
         Intent i = new Intent(this, Main2Activity.class);
-        i.putExtra("dato", et1.getText().toString());
+        //i.putExtra("dato", et1.getText().toString());
         startActivity(i);
     }
 }
